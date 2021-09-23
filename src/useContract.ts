a799275a0cbfb2d20280fb4386b07c6c545922c6
sam bacha <sam@manifoldfinance.com>
2021-09-23 00:46:33 -0700

    Create useContract.ts

---
+++ src/useContract.ts
@@ -0,0 +1,24 @@
+
+
+MULTICALL_ADDRESS='0x...'
+// returns null on errors
+export function useContract<T extends Contract = Contract>(
+  addressOrAddressMap: string | { [chainId: number]: string } | undefined,
+  ABI: any,
+  withSignerIfPossible = true
+): T | null {
+  const { library, account, chainId } = useActiveWeb3React()
+  return useMemo(() => {
+    if (!addressOrAddressMap || !ABI || !library || !chainId) return null
+    let address: string | undefined
+    if (typeof addressOrAddressMap === 'string') address = addressOrAddressMap
+    else address = addressOrAddressMap[chainId]
+    if (!address) return null
+    try {
+      return getContract(address, ABI, library, withSignerIfPossible && account ? account : undefined)
+    } catch (error) {
+      console.error('Failed to get contract', error)
+      return null
+    }
+  }, [addressOrAddressMap, ABI, library, chainId, withSignerIfPossible, account]) as T
+}
